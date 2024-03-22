e688ddfbf660d72ea99713a96ac319944e71eac1
SteelBlueZK <sbluefaf@gmail.com>
2024-03-22 16:11:07 -0400

    Trivial prime factors

--- rsa.py
+++ rsa.py
@@ -9,3 +9,16 @@ def factor(num):
         return [1, num]
     factors.append(num)
     return factors;
+
+def prime_factors_of(number):
+	divisor = 2
+	remaining = number
+	prime_factors = list()
+	while(divisor <= remaining):
+		if remaining % divisor == 0:
+			prime_factors.append(divisor)
+			remaining = remaining // divisor
+		else:
+			divisor += 1
+	return prime_factors
+
