ba070e7844f756bc59f6aecfb209d1549e5e351b
ImaHippu <nattang717@gmail.com>
2024-03-22 16:10:39 -0400

    number to num
    test

--- rsa.py
+++ rsa.py
@@ -1,11 +1,11 @@
-def factor(number):
+def factor(num):
     count = 0
     factors = [1]
-    for i in range(2, int(number / 2)):
-        if number % i == 0:
+    for i in range(2, int(num / 2)):
+        if num % i == 0:
             count += 1
             factors.append(i)
     if count == 0:
-        return [1, number]
-    factors.append(number)
+        return [1, num]
+    factors.append(num)
     return factors;
