96da6b67ac07126fc1cf7a321d5697c812f80de4
Matthew Greene <matthewgreene0204@gmail.com>
2024-03-29 13:23:51 -0400

    pollard rho given better optional args

--- pollard.py
+++ pollard.py
@@ -3,15 +3,11 @@ from euclid import gcd
 from rsa import is_prime
 from random import randrange
 
-def pollard_rho(number: int, g=None):
+def pollard_rho(number: int, g_a=1, g_b=1):
     if is_prime(number):
         return None
-    b = 0
-    def base_g(value):
-        return (value**2 + 1) % number
-    
-    if g is None:
-        g = base_g
+    def g(value, a=g_a, b=g_b):
+        return (a*value**2 + b) % number
     
     x = randrange(2,number)
     y = x
@@ -25,8 +21,15 @@ def pollard_rho(number: int, g=None):
         return None
     return d
 
+from rsa import next_prime
+from time import time
 if __name__ == '__main__':
-    n = 9209839122440374002906008377605580208264841025166426304451583112053
-    rho = pollard_rho(n)
+    #n = 9209839122440374002906008377605580208264841025166426304451583112053
+    n = next_prime(10**11*4) * next_prime(10**20*493)
+    start_time = time()
+    rho = pollard_rho(n, g_a=1, g_b=1)
+    end_time = time()
     print("factor of n is: ", rho)
     print("if blank, pollard failed")
+    print("n = ",n)
+    print("runtime:", end_time - start_time)
