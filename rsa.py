4572f15e6c55a4e1580971b9acc8292b8f2757b5
SteelBlueZK <sbluefaf@gmail.com>
2024-03-29 00:24:12 -0400

    next_prime function added to rsa.py

--- rsa.py
+++ rsa.py
@@ -47,6 +47,13 @@ def is_prime(number, test_count=5):
             return False
     return True
 
+def next_prime(n):
+    if n % 2 == 0:
+        n += 1
+    while not is_prime(n):
+        n += 2
+    return n
+
 def pollard_rho_helper(value, exponent, constant, n):
     var_i = (value**exponent + constant) % n
     return var_i
