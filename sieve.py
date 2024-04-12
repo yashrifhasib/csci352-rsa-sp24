efcabeb05987db7d6ef81ee92beb948363ff3d8e
Matthew Greene <matthewgreene0204@gmail.com>
2024-04-12 17:44:00 -0400

    sieve fix for imports

--- sieve.py
+++ sieve.py
@@ -7,7 +7,7 @@ from squarerootAttack import real_sqrt as sqrt
 
 FACTOR_BASE_FILE = "factor_base_13_new.txt"
 
-def create_offsets(modulus: int, prime: int) -> list[int]:
+def create_offsets(modulus: int, prime: int, start_number: int) -> list[int]:
     """This function runs tonelli shanks and
     modifies the outputs to work with the QS"""
     tonelli = tonelli_shanks(modulus, prime)
@@ -16,13 +16,13 @@ def create_offsets(modulus: int, prime: int) -> list[int]:
         offsets.append((t - start_number)%prime)
     return offsets
 
-def reduce_sieve(sieve: list[int], modulus: int, factorBase: list[int]) -> None:
+def reduce_sieve(sieve: list[int], modulus: int, factorBase: list[int], start_number: int) -> None:
     """This function modifies the sieve input,
     resulting in a value of 1 if it's smooth with
     respect to the factor base and modulus supplied."""
     # TODO: implement special -1 of factorbase
     for prime in factorBase[1:]:
-        offsets = create_offsets(modulus, prime)
+        offsets = create_offsets(modulus, prime, start_number)
         for o in offsets:
             for i in range(o, len(sieve), prime):
                 while sieve[i] % prime == 0:
@@ -43,7 +43,7 @@ def get_relaitons(start_number: int, end_number: int, composite: int, factorBase
     with respect to a factor base and a composite number to factor."""
     base_numbers = list(range(start_number, end_number, 1))
     sieve = [pow(x,2,composite) for x in base_numbers]
-    reduce_sieve(sieve, composite, factorBase)
+    reduce_sieve(sieve, composite, factorBase, start_number)
     return give_row_numbers(base_numbers, sieve)
 
 if __name__ == "__main__":
@@ -53,8 +53,8 @@ if __name__ == "__main__":
     # N = 9209839122440374002906008377605580208264841025166426304451583112053
     N = 227179
     
-    start_number = sqrt(N) + 300
-    end_number = start_number + 10000
+    start_number = sqrt(N)-100
+    end_number = start_number +1000
     relations = get_relaitons(start_number, end_number, N, factorBase)
     
     print("The following is the range of numbers on the left, and the modulus quadratic on the right:")
