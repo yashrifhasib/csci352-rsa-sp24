717704339eb77d57e3f7456c6bf62b826bbf9a7f
Matthew Greene <matthewgreene0204@gmail.com>
2024-04-12 10:58:54 -0400

    documentation for pollard.h fn :3

--- pollardRho.h
+++ pollardRho.h
@@ -2,4 +2,10 @@
 
 #include <gmpxx.h>
 
+/**
+ * @brief pollard rho that returns a single factor of input.
+ * 
+ * @param input this number must be composite, undefined when prime.
+ * @return mpz_class some factor of input
+ */
 mpz_class pollard(const mpz_class& input);
