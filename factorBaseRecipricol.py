991de2f6f62dcfec9979be5c9355394076c86cde
Matthew Greene <matthewgreene0204@gmail.com>
2024-04-12 17:38:57 -0400

    added function, added docstrings

--- factorBaseRecipricol.py
+++ factorBaseRecipricol.py
@@ -1,8 +1,5 @@
-from math import ceil,sqrt,log
 from time import time
-from random import randint
-import math
-
+from rsa import is_prime, prev_prime, next_prime
 
 def get_primesDict_frm_file(bound):
     primes = dict()
@@ -12,42 +9,6 @@ def get_primesDict_frm_file(bound):
             primes[i] = int(p)
             p = primeFile.readline()
     return primes
-    
-    
-
-def is_prime_helper(n):
-    # miller rabine implementation
-    if n == 2:
-        return True
-    if n % 2 == 0 or n < 3:
-        return False
-    # n = pow(2,k) * m
-    k = 0
-    m = n - 1
-    while m%2 == 0:
-        k += 1
-        m = m // 2
-    if True:
-        a = randint(2,n-1)
-        # b starts at b0
-        b = pow(a,m,n)
-        if b in [1, n-1]:
-            return True
-        # check b1 - b(k-1)
-        for b_count in range(1,k):
-            b = pow(b, 2, n)
-            # check if miller-rabin condition
-            if b == 1:
-                return False
-            if b == n-1:
-                return True
-    return False
-
-def is_prime(number, test_count=5):
-    for x in range(test_count):
-        if not is_prime_helper(number):
-            return False
-    return True
 
 def prime_factors(n):
     factors = {}
@@ -86,30 +47,32 @@ def legendre(top,bottom):
         return legendre(bottom,top)
     elif(top%4 == 3 and bottom%4 == 3):
         return (-1)*legendre(bottom,top)
+
+def create_factor_base(prime_smoothness, n):
+    """
+    ### creates factor base file according to naming convention
     
+    arguments:
 
-def next_prime(n):
-    if n % 2 == 0:
-        n += 1
-    while not is_prime(n):
-        n += 2
-    return n
+    prime_smoothness -- the index of the largest prime in the list of all prime numbers
 
-def create_factor_base(B, n):
-    primes = get_primesDict_frm_file(2*B)
+    n -- the number we want to factor
+    """
+    primes = get_primesDict_frm_file(2*prime_smoothness)
     factorBase = list()
     factorBase.append(-1)
     # factorBase.append(2)
     # factorBase.append(3)
     
-    i = 0
-    while i < B:
-        p = primes[i]
-        if legendre(n, primes[i])==1:
+    prime_count = 0
+    while prime_count < prime_smoothness:
+        p = primes[prime_count]
+        if legendre(n, primes[prime_count])==1:
         # if pow(n,(p-1)//2,p) == 1:
             factorBase.append(p)
-        i+=1
-    print(f"created factor base with {len(factorBase)} length and searched {i} numbers")
+        prime_count+=1
+    factor_base_size = len(factorBase)
+    print(f"created factor base with {factor_base_size} length and searched {prime_count} numbers")
     return factorBase
 
 def save_list_to_file(myList):
@@ -118,6 +81,31 @@ def save_list_to_file(myList):
         for p in myList:
             file.write(str(p)+'\n')
     print("wrote to file", name)
+
+def highest_prime_order(max_value):
+    """ find the prime below input and returns
+    what number prime it is."""
+    if not is_prime(max_value):
+        max_value = prev_prime(max_value)
+    count = 1
+    prime = 2
+    while prime < max_value:
+        count += 1
+        prime = next_prime(prime+1)
+    return count
+
+def create_factor_base_from_absolute(absolute_smoothness, n):
+    """
+    ### creates factor base file according to naming convention
+
+    arguments:
+
+    absolute_smoothness -- the upper bound for numbers in the factor base
+
+    n -- the number we want to factor
+    """
+    prime_smoothness = highest_prime_order(absolute_smoothness)
+    return create_factor_base(prime_smoothness, n)
 
 if __name__ == '__main__':
 
