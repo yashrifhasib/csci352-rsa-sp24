2bfd6c2ebaa3a25ecf7576ac5ee62990f52e1de2
Matthew Greene <matthewgreene0204@gmail.com>
2024-04-12 14:48:29 -0400

    quadratic sieve is a one function wonder

--- quadratic_sieve.py
+++ quadratic_sieve.py
@@ -1,36 +1,39 @@
+import time
 from math import log,e
+from pprint import pprint
 from TonelliShanks import tonelli_shanks
 from squarerootAttack import real_sqrt as sqrt
-from pprint import pprint
-import time
 
 FACTOR_BASE_FILE = "factor_base_13_new.txt"
 
-Q = dict()
-def compute_sequence(B,x,N):
-    for i in range(20):
-        # Q_i 
-        Q[i] = (x+i)**2 - N
-    
-    
-def sieve(step,p):
-    i = step
-    while i < len(Q):
-        print(i)
-        Q[i] = Q[i] // p
-        i += p
-    print(list(Q.values()))
+def create_offsets(modulus, prime):
+    tonelli = tonelli_shanks(modulus, prime)
+    offsets = list()
+    for t in tonelli:
+        offsets.append((t - start_number)%prime)
+    return offsets
 
-# TODO make this function work -Matthew
-def reduce_sieve(mysieve, sequence, modulus, factorBase):
+def reduce_sieve(sieve, modulus, factorBase):
     # TODO: implement special -1 of factorbase
-    start_number = sequence[0]
     for prime in factorBase[1:]:
-        offset = start_number % prime
-        for i in range(offset-1, len(mysieve), prime):
-            before = mysieve[i]
-            mysieve[i] //= prime
-            print('pre:',before,'sequence#:',sequence[i], ",", mysieve[i], 'prime', prime,'index',i)
+        offsets = create_offsets(modulus, prime)
+        for o in offsets:
+            for i in range(o, len(sieve), prime):
+                while sieve[i] % prime == 0:
+                    sieve[i] //= prime
+
+def give_row_numbers(base_numbers, sieve):
+    ret = list()
+    for entry in zip(base_numbers, sieve):
+        if entry[1] == 1:
+            ret.append(entry[0])
+    return ret
+
+def get_relaitons(start_number, end_number, modulus, factorBase):
+    base_numbers = list(range(start_number, end_number, 1))
+    sieve = [pow(x,2,modulus) for x in base_numbers]
+    reduce_sieve(sieve, modulus, factorBase)
+    return give_row_numbers(base_numbers, sieve)
 
 if __name__ == "__main__":
     factorBase = None
@@ -41,27 +44,9 @@ if __name__ == "__main__":
     
     start_number = sqrt(N)
     end_number = start_number + 20
-    base_numbers = list(range(start_number, end_number, 1))
-    mysieve = [pow(x,2,N) for x in base_numbers]
+    relations = get_relaitons(start_number, end_number, N, factorBase)
     
-    pprint(list(zip(base_numbers, mysieve)))
-    # reduce_sieve(mysieve, base_numbers, N, factorBase)
-    
-    compute_sequence(len(factorBase), start_number, N)
-    # first, iterate through primes
-    #   solve for n to see what primes in factor base to sieve using shanks tonelli
-    #   (x + n)^2 = N mod p_i
-    #   (x + n) = sqrt(N) mod p_i
-    #   n =  sqrt(N) - x mod p_i
-    for p in factorBase:
-        if p == -1:
-            continue
-        square1,square2 = tonelli_shanks(N, p)
-        n1 = (square1 - start_number ) % p
-        n2 = (square2 - start_number ) % p
-        if(n1 == n2):
-            #only need to pass once
-            sieve(n1,p)
-        else:
-            sieve(n1,p)
-            sieve(n2,p)
+    print("The following is the range of numbers on the left, and the modual quadratic on the right:")
+    pprint(list(zip(list(range(start_number, end_number, 1)), [pow(x,2,N) for x in list(range(start_number, end_number, 1))])))
+    print("The following is the relations we have discovered via sieving:")
+    pprint(relations)
