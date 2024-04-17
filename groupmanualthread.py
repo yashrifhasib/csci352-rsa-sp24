60cdec056dfc7af30bf32e38ff26835ac96fc477
Russgee <russtgo30@gmail.com>
2024-04-16 20:49:19 -0400

    Debugged groupmanualthread

--- groupmanualthread.py
+++ groupmanualthread.py
@@ -4,8 +4,8 @@ from factorBaseRecipricol import create_factor_base_from_absolute
 from sieve import get_relaitons
 from squarerootAttack import real_sqrt as sqrt
 
-def create_output(name_seed, myrelations):
-    with open("work/work"+str(name_seed)+".txt", 'w', encoding='utf-8') as file:
+def create_output(name_seed, factorbase_length, myrelations):
+    with open("work/work"+str(factorbase_length)+"_"+str(name_seed)+".txt", 'w', encoding='utf-8') as file:
         for number in myrelations:
             file.write(str(number)+'\n')
 
@@ -22,20 +22,20 @@ if __name__ == '__main__':
     #factor base
     
     absolute_smoothness, _ = dickman_rho_best_smoothness(N)
-    factorBase = create_factor_base_from_absolute(int(absolute_smoothness), N)
+    factorBase = create_factor_base_from_absolute(int(absolute_smoothness*.7), N)
 
-    relations = list()
-
+    interval = 5_000_000
+    iterations = 12
+    # arg1 = 0, arg2 = 1
     for x in range(arg1, arg2):
-        interval = 5_000_000
-        iterations = 12
+        relations = list()
         offset = x * iterations * interval
         start = sqrt(N) + offset
         print("start sieving")
         for _ in range(iterations):
             print("sieving: ",start)
             end = start + interval
-            relations = get_relaitons(start, end, N, factorBase)
+            relations += get_relaitons(start, end, N, factorBase)
             start = end
         print("end sieving, new found relations:", len(relations))
-        create_output(x, relations)
+        create_output(x, len(factorBase),relations)
