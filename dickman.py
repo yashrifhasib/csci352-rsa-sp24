10754b35205546e8609ed03159c97dc2a7a6c508
Matthew Greene <matthewgreene0204@gmail.com>
2024-04-18 04:48:04 -0400

    dickman main creates plot; shorter lines

--- dickman.py
+++ dickman.py
@@ -1,17 +1,21 @@
-import math
+
+from math import sqrt, log
+from math import e as math_e
+import matplotlib.pyplot as plt
 
 def rho(i):
     return 1/(i**(i))
 
 def aproximate_search_range_size(smooth, x, F):
-    x = math.sqrt(2*x);
+    x = sqrt(2*x);
     
-    count = rho(math.log(x)/math.log(smooth)) * x
-    numsearch = int((1/(count/x))*smooth/(2*math.log(F)))
+    count = rho(log(x)/log(smooth)) * x
+    numsearch = int((1/(count/x))*smooth/(2*log(F)))
     return numsearch
 
 def print_dickman_rho_data(n):
-    F = math.e ** (  math.sqrt(math.log(n) * math.log(math.log(n)) ))
+    # F is the squareroot of e to the power of ( square root of n times sqrt of sqrt of n )
+    F = math_e ** (sqrt(log(n) * log(log(n))))
     F = F**(1/2)
     smoothness = 10
     exponent = 2
@@ -24,6 +28,25 @@ def print_dickman_rho_data(n):
     print("function minimum absolute smoothness:",smoothness)
     for _ in range(smoothness//4, smoothness*2+1, smoothness//4):
         print("smoothness:", _, "search size:", aproximate_search_range_size(_, n, F))
+    
+    figure = plt.figure()
+    x = list(range(smoothness//10, smoothness*4+1, smoothness//10))
+    y = [aproximate_search_range_size(val, n, F) for val in x]
+    # x = [val/100_000_000 for val in x]
+    # y = [val/179687 for val in y]
+    plt.xlabel("absolute smoothness parameter")
+    plt.ylabel("approximate search size")
+    plt.plot(x, y)
+    plt.xlim(0,8_000_000)
+    plt.ylim(0, 1_000_000_000)
+    plt.ticklabel_format(style='sci', axis='both', scilimits=(0,0))
+    plt.gca().ticklabel_format(axis='both', style='sci', scilimits=(0,0), useMathText=True)
+    plt.gca().ticklabel_format(axis='both', style='sci', scilimits=(0,0), useMathText=True)
+
+    print(list(zip(x,y)))
+    plt.show()
+    
+    
 
 def dickman_rho_best_smoothness(n):
     """
@@ -37,7 +60,7 @@ def dickman_rho_best_smoothness(n):
 
     aprox.search range -- aproximate search range of the optimal smoothness
     """
-    F = math.e ** (  math.sqrt(math.log(n) * math.log(math.log(n)) ))
+    F = math_e ** (  sqrt(log(n) * log(log(n)) ))
     F = F**(1/2)
     smoothness = 10
     exponent = 2
